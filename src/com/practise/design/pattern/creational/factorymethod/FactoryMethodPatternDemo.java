e144c5b0b3fada90db63739b949a4c4a5e324c42
tribles <senthilkumar.s.shanmugam@oracle.com>
2018-08-18 20:58:56 +0530

    gang of four design pattern

---
+++ src/com/practise/design/pattern/creational/factorymethod/FactoryMethodPatternDemo.java
@@ -0,0 +1,18 @@
+package com.practise.design.pattern.creational.factorymethod;
+
+/**
+ * Typical usage of factory method pattern by clients
+ * they want products, dont need to know what type of products
+ * so they dont tied to the exact product type
+ * @author seshshan
+ *
+ */
+public class FactoryMethodPatternDemo {
+
+	public static void main(String[] args) {
+     AbstractCreator factory = new ConcreteCreator();
+     AbstractProduct product = new ConcreteProduct();
+     product.feature();
+	}
+
+}
